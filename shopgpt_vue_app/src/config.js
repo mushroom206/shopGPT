b911d8de8ac123d316e69231be7d23dd47a4a343
Yumin Gong <gym0419@gmail.com>
2023-05-23 16:07:22 -0700

    test api url

--- shopgpt_vue_app/src/config.js
+++ shopgpt_vue_app/src/config.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://shopgpt-cloudbuild-service-7bz57dofha-wl.a.run.app' 
-  : 'http://localhost:6000';
+  ? 'https://shopgpt-cloudbuild-service-7bz57dofha-wl.a.run.app/api/' 
+  : 'http://localhost:5000/api/';
 
 module.exports = { API_URL };
 
